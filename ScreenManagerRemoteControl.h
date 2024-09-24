18f76f06f16b07f722534eaaa2d0fbb211777181
shengelenge <nicolas@wielocha.pl>
2024-09-24 11:45:48 +0200

    Add ScreenManagerRemoteControl interface

---
+++ ScreenManagerRemoteControl.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include <vector>
+// #include "GameObject.h"
+// #include "GameObjectSharer.h"
+
+class ScreenManagerRemoteControl {
+public:
+	virtual void SwitchScreens(std::string screenToSwitchTo) = 0;
+	virtual void loadLevelInPlayMode(std::string screenToLoad) = 0;
+	// virtual std::vector<GameObject>& getGameObjects() = 0;
+	// virtual GameObjectSharer& shareGameObjectSharer() = 0;
+};
+
