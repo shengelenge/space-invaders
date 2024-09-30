faf10c912c8ec1bd7c07f6950fac2d910f37be2a
shengelenge <nicolas@wielocha.pl>
2024-09-30 14:16:58 +0200

    Add ObjectsTags class

---
+++ ObjectTags.cpp
@@ -0,0 +1,21 @@
+#include "ObjectTags.h"
+#include "DevelopState.h"
+
+const std::string ObjectTags::START_OF_OBJECT = "[START OBJECT]";
+const std::string ObjectTags::END_OF_OBJECT = "[END OBJECT]";
+const std::string ObjectTags::COMPONENT = "[COMPONENT]";
+const std::string ObjectTags::COMPONENT_END = "[-COMPONENT]";
+const std::string ObjectTags::NAME = "[NAME]";
+const std::string ObjectTags::NAME_END = "[-NAME]";
+const std::string ObjectTags::WIDTH = "[WIDTH]";
+const std::string ObjectTags::WIDTH_END = "[-WIDTH]";
+const std::string ObjectTags::HEIGHT = "[HEIGHT]";
+const std::string ObjectTags::HEIGHT_END = "[-HEIGHT]";
+const std::string ObjectTags::LOCATION_X = "[LOCATION X]";
+const std::string ObjectTags::LOCATION_X_END = "[-LOCATION X]";
+const std::string ObjectTags::LOCATION_Y = "[LOCATION Y]";
+const std::string ObjectTags::LOCATION_Y_END = "[-LOCATION Y]";
+const std::string ObjectTags::BITMAP_NAME = "[BITMAP NAME]";
+const std::string ObjectTags::BITMAP_NAME_END = "[-BITMAP NAME]";
+const std::string ObjectTags::ENCOMPASSING_RECT_COLLIDER = "[ENCOMPASSING RECT COLLIDER]";
+const std::string ObjectTags::ENCOMPASSING_RECT_COLLIDER_END = "[-ENCOMPASSING RECT COLLIDER]";
---
+++ ObjectTags.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+class ObjectTags {
+public:
+	static const std::string START_OF_OBJECT;
+	static const std::string END_OF_OBJECT;
+	static const std::string COMPONENT;
+	static const std::string COMPONENT_END;
+	static const std::string NAME;
+	static const std::string NAME_END;
+	static const std::string WIDTH;
+	static const std::string WIDTH_END;
+	static const std::string HEIGHT;
+	static const std::string HEIGHT_END;
+	static const std::string LOCATION_X;
+	static const std::string LOCATION_X_END;
+	static const std::string LOCATION_Y;
+	static const std::string LOCATION_Y_END;
+	static const std::string BITMAP_NAME;
+	static const std::string BITMAP_NAME_END;
+	static const std::string ENCOMPASSING_RECT_COLLIDER;
+	static const std::string ENCOMPASSING_RECT_COLLIDER_END;
+}
