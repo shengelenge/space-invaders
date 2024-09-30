c3d3d7bc893514c52f5095472a738eae2745aa4d
shengelenge <nicolas@wielocha.pl>
2024-10-01 00:24:28 +0200

    Update GameInputHandler class

--- GameInputHandler.cpp
+++ GameInputHandler.cpp
@@ -2,6 +2,8 @@
 #include "SoundEngine.h"
 #include "GameScreen.h"
 
+class BulletSpawner;
+
 void GameInputHandler::initialize() {}
 
 void GameInputHandler::handleGamepad() {}
@@ -12,6 +14,41 @@ void GameInputHandler::handleKeyPressed(sf::Event& event, sf::RenderWindow& wind
 		SoundEngine::playClick();
 		getPtrToScreenManagerRemoteControl()->SwitchScreens("Select");
 	}
+
+	if (event.key.code == sf::Keyboard::Left) {
+		m_PlayerUpdateComponent->moveLeft();
+	}
+
+	if (event.key.code == sf::Keyboard::Right) {
+		m_PlayerUpdateComponent->moveRight();
+	}
+
+	if (event.key.code == sf::Keyboard::Up) {
+		m_PlayerUpdateComponent->moveUp();
+	}
+
+	if (event.key.code == sf::Keyboard::Down) {
+		m_PlayerUpdateComponent->moveDown();
+	}
 }
 
-void GameInputHandler::handleKeyReleased(sf::Event& event, sf::RenderWindow& window) {}
+void GameInputHandler::handleKeyReleased(sf::Event& event, sf::RenderWindow& window) {
+	if (event.key.code == sf::Keyboard::Left) {
+		m_PlayerUpdateComponent->stopLeft();
+	} else if (event.key.code == sf::Keyboard::Right) {
+		m_PlayerUpdateComponent->stopRight();
+	} else if (event.key.code == sf::Keyboard::Up) {
+		m_PlayerUpdateComponent->stopUp();
+	} else if (event.key.code == sf::Keyboard::Down) {
+		m_PlayerUpdateComponent->stopDown();
+	} else if (event.key.code == sf::Keyboard::Space) {
+		// Shoot a bullet
+		SoundEngine::playShoot();
+		sf::Vector2f spawnLocation;
+		
+		spawnLocation.x = m_PlayerTransformComponent->getLocation().x + m_PlayerTransformComponent->getSize().x / 2;
+		spawnLocation.y = m_PlayerTransformComponent->getLocation().y;
+
+		static_cast<GameScreen*>(getmParentScreen())->spawnBullet(spawnLocation, true);
+	}
+}
--- GameInputHandler.h
+++ GameInputHandler.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "InputHandler.h"
+#include "PlayerUpdateComponent.h"
+#include "TransformComponent.h"
 
 class GameScreen;
 
 class GameInputHandler : public InputHandler {
+private:
+	std::shared_ptr<PlayerUpdateComponent> m_PlayerUpdateComponent;
+	std::shared_ptr<TransformComponent> m_PlayerTransformComponent;
+
+	bool m_ButtonPressed = false;
+
 public:
 	void initialize();
 	void handleGamepad() override;
