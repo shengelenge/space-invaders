25e9f66aa81a200b68746e12d47fd4da6ab8c6ab
shengelenge <nicolas@wielocha.pl>
2024-09-30 23:05:44 +0200

    Add PlayModeObjectLoader class

---
+++ PlayModeObjectLoader.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include "PlayModeObjectLoader.h"
+#include "ObjectTags.h"
+
+void PlayModeObjectLoader::loadGameObjectForPlayMode(std::string pathToFile, std::vector<GameObject>& gameObjects) {
+	std::ifstream reader(pathToFile);
+	std::string lineFromFile;
+
+	float x = 0, y = 0, width = 0, height = 0;
+	std::string value = "";
+	while (std::getline(reader, lineFromFile)) {
+		if (lineFromFile.find(ObjectTags::START_OF_OBJECT) != std::string::npos) {
+			GameObjectBlueprint bp;
+			m_BlueprintObjectParser.parseNextObjectForBlueprint(reader, bp);
+			m_GameObjectFactoryPlayMode.buildGameObject(bp, gameObjects);
+		}
+	}
+}
---
+++ PlayModeObjectLoader.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "GameObject.h"
+#include "BlueprintObjectParser.h"
+#include "GameObjectFactoryPlayMode.h"
+
+class PlayModeObjectLoader {
+private:
+	BlueprintObjectParser m_BlueprintObjectParser;
+	GameObjectFactoryPlayMode m_GameObjectFactoryPlayMode;
+
+public:
+	void loadGameObjectForPlayMode(std::string pathToFile, std::vector<GameObject>& mGameObjects);
+};
+
