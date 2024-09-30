d2f2225ae0ba8c408a8f3bfe65672cee3eebaa12
shengelenge <nicolas@wielocha.pl>
2024-09-30 23:44:47 +0200

    Update ScreenManager header

--- ScreenManager.h
+++ ScreenManager.h
@@ -1,17 +1,17 @@
 #pragma once
-#include "SFML/Graphics.hpp"
+#include <SFML/Graphics.hpp>
 #include <map>
+#include <iostream>
 #include "GameScreen.h"
 #include "ScreenManagerRemoteControl.h"
 #include "SelectScreen.h"
-// #include "LevelManager.h"
+#include "LevelManager.h"
 #include "BitmapStore.h"
-#include <iostream>
 
 class ScreenManager : public ScreenManagerRemoteControl {
 private:
 	std::map<std::string, std::unique_ptr<Screen>> m_Screens;
-	// LevelManager m_LevelManager;
+	LevelManager m_LevelManager;
 
 protected:
 	std::string m_CurrentScreen = "Select";
@@ -29,23 +29,24 @@ public:
 	From ScreenManagerRemoteControl interface
 	********************************************************
 	********************************************************/
+
 	void ScreenManagerRemoteControl::SwitchScreens(std::string screenToSwitchTo) {
 		m_CurrentScreen = "" + screenToSwitchTo;
 		m_Screens[m_CurrentScreen]->initialize();
 	}
 
 	void ScreenManagerRemoteControl::loadLevelInPlayMode(std::string screenToLoad) {
-		// m_LevelManager.getGameObjects().clear();
-		// m_levelManager.laodGameObjectsForPlayMode(screenToLoad);
+		m_LevelManager.getGameObjects().clear();
+		m_LevelManager.loadGameObjectsForPlayMode(screenToLoad);
 		SwitchScreens("Game");
 	}
 
-	//vector<GameObject>& ScreenManagerRemoteControl::getGameObjects() {
-	//	return m_LevelManager.getGameObjects();
-	//}
+	std::vector<GameObject>& ScreenManagerRemoteControl::getGameObjects() {
+		return m_LevelManager.getGameObjects();
+	}
 
-	//GameObjectSharer& shareGameObjectSharer() {
-	//	return m_LevelManager;
-	//}
+	GameObjectSharer& shareGameObjectSharer() {
+		return m_LevelManager;
+	}
 };
 
